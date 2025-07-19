d5adeea8e671ffa237f713f9d657015ebf2c2b40
Ari Waingold <ari.waingold@outlook.com>
2025-07-18 23:43:58 -0700

    auth pt2 forgot to commit some stuff bcs i was in the wrong directory

---
+++ context/AuthContext.jsx
@@ -0,0 +1,44 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from 'firebase/auth';
+import { app } from '../lib/firebase';
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+    
+    const auth = getAuth(app);
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    });
+
+    const login = async (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    const signUp = async (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    const logout = async () => {
+        return signOut(auth);
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, login, signUp, logout, loading }}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+    
+}
